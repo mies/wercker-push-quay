3590510e36ba73eab8fabf5e37f9b9deae0d91c8
Benno van den Berg <bennovandenberg@gmail.com>
2015-04-28 15:31:01 +0200

    Serve actual JSON

--- main.go
+++ main.go
@@ -6,17 +6,30 @@ import (
 	"net/http"
 )
 
+type CitiesResponse struct {
+	Cities []string `json:"cities"`
+}
+
 func CityHandler(res http.ResponseWriter, req *http.Request) {
-	data, _ := json.Marshal("{'cities':'San Francisco, Amsterdam, Berlin, New York'}")
+	citiesResponse := &CitiesResponse{
+		Cities: []string{
+			"San Francisco",
+			"Amsterdam",
+			"Berlin",
+			"New York",
+		},
+	}
+	data, _ := json.MarshalIndent(citiesResponse, "", "  ")
 	res.Header().Set("Content-Type", "application/json; charset=utf-8")
 	res.Write(data)
 }
 
 func main() {
+	log.Println("Listening on this host: http://localhost:5000")
+
 	http.HandleFunc("/cities.json", CityHandler)
 	err := http.ListenAndServe(":5000", nil)
 	if err != nil {
-		log.Fatal("ListenAndServe: ", err)
+		log.Fatal("Unable to listen on :5000: ", err)
 	}
-	log.Println("Listening on this host: http://localhost:5000")
 }
